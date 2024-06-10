e197c830ab68cda936bab8e7c1a4016a5a1b226c
Azamkhodja <azamxoja0091@gmail.com>
2024-06-10 18:41:58 +0500

    added date in models.py

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class Contact(models.Model):
     name = models.TextField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     message = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} - {self.date.year}.{self.date.month}.{self.date.day} - {self.date.hour}:{self.date.minute}'
