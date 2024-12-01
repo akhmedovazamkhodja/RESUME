d8256520e7f64e914bfae545144ed0a7c67400e3
akhmedovazamkhodja <azamxoja0091@gmail.com>
2024-12-01 23:52:41 +0500

    add success tag

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,12 @@ from .forms import ContactForm
 def index(request):
     myworks = MyWork.objects.all()
     contact = Contact.objects.all()
+    is_success = request.session.pop('is_success', False)
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         Contact.objects.create(name=name, email=email, message=message)
+        is_success = True
         return redirect('index')
-    return render(request, 'index.html', {'myworks': myworks})
+    return render(request, 'index.html', {'myworks': myworks, 'is_success': is_success})
